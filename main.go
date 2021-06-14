759a21cad760e72f00f12500f4fa7fbe58d23b43
ysy <ysy20121223@live.com>
2021-06-14 17:55:32 +0900

    up

--- main.go
+++ main.go
@@ -12,19 +12,20 @@ import (
 // "cdtuhgrbalryenwl"
 func main() {
 	//from := flag.String("from", "test@d.a", "send from")
-	toList := strings.Split(*(flag.String("to", "test@d.c;test@d.d", "send to")), ";")
+	to := flag.String("to", "test@d.c;test@d.d", "send to")
 	num := flag.Int("num", 1, "how many to send")
 	u := flag.String("u", "", "username: xxx@yahoo.com")
 	p := flag.String("p", "", "generated app password")
-	body := []byte(*flag.String("body", "Hello geeks!!!", "send mail body"))
+	body := []byte(*(flag.String("body", "Hello geeks!!!", "send mail body")))
+	host := flag.String("hp", "smtp.mail.yahoo.com:587", "host+port")
 
 	flag.Parse()
-	host := "smtp.mail.yahoo.com"
-	port := "587"
-	auth := smtp.PlainAuth("", *u, *p, host)
+	toList := strings.Split(*to, ";")
+	auth := smtp.PlainAuth("", *u, *p, strings.Split(*host, ":")[0])
 	fmt.Println(auth)
+	fmt.Println(toList)
 	for i := 0; i < *num; i++ {
-		err := smtp.SendMail(host+":"+port, auth, *u, toList, body)
+		err := smtp.SendMail(*host, auth, *u, toList, body)
 
 		// handling the errors
 		if err != nil {
