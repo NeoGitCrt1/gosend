e5e5db542643ee820fc9e41f2ddc039f623be198
ysy <ysy20121223@live.com>
2021-06-14 14:30:25 +0900

    u

--- main.go
+++ main.go
@@ -5,30 +5,35 @@ import (
 	"fmt"
 	"net/smtp"
 	"strings"
+	"time"
 	_ "time"
 )
+
 // "cdtuhgrbalryenwl"
 func main() {
-	from := flag.String("from", "test@d.a", "send from")
-	toList := strings.Split(*(flag.String("out", "test@d.c;test@d.d", "send to")), ";")
-	num := flag.Int("send num", 1, "how many to send")
-	u := flag.String("user", "", "username: xxx@yahoo.com")
-	p := flag.String("app password", "", "generated app password")
+	//from := flag.String("from", "test@d.a", "send from")
+	toList := strings.Split(*(flag.String("to", "test@d.c;test@d.d", "send to")), ";")
+	num := flag.Int("num", 1, "how many to send")
+	u := flag.String("u", "", "username: xxx@yahoo.com")
+	p := flag.String("p", "", "generated app password")
+	body := []byte(*flag.String("body", "Hello geeks!!!", "send mail body"))
+
+	flag.Parse()
 	host := "smtp.mail.yahoo.com"
 	port := "587"
-	body := []byte(*flag.String("body", "Hello geeks!!!", "send mail body"))
 	auth := smtp.PlainAuth("", *u, *p, host)
-	
-	for i := 0 ; i < *num; i++ {
-		err := smtp.SendMail(host+":"+port, auth, *from, toList, body)
+	fmt.Println(auth)
+	for i := 0; i < *num; i++ {
+		err := smtp.SendMail(host+":"+port, auth, *u, toList, body)
 
 		// handling the errors
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
+
+		time.Sleep(500 * time.Millisecond)
 	}
-	
 
 	fmt.Println("end")
 }
