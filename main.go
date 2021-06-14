1f10a11147c6d649ba17883c3c6a422bbf32a1a6
NeoGitCrt1 <ysy20121223@live.com>
2021-06-14 14:15:00 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,26 +1,34 @@
 package main
+
 import (
+	"flag"
 	"fmt"
 	"net/smtp"
+	"strings"
 	_ "time"
 )
-
+// "cdtuhgrbalryenwl"
 func main() {
-	from := "yusyang20121223@yahoo.com"
-	toList := []string{"yusy20121223@gmail.com"}
-  
+	from := flag.String("from", "test@d.a", "send from")
+	toList := strings.Split(*(flag.String("out", "test@d.c;test@d.d", "send to")), ";")
+	num := flag.Int("send num", 1, "how many to send")
+	u := flag.String("user", "", "username: xxx@yahoo.com")
+	p := flag.String("app password", "", "generated app password")
 	host := "smtp.mail.yahoo.com"
 	port := "587"
-	msg := "Hello geeks!!!"
-	body := []byte(msg)
-	auth := smtp.PlainAuth("", "yusyang20121223@yahoo.com", "cdtuhgrbalryenwl", host)
-	err := smtp.SendMail(host+":"+port, auth, from, toList, body)
-  
-    // handling the errors
-    if err != nil {
-        fmt.Println(err)
-	return
-    }
-  
-    fmt.Println("end")
+	body := []byte(*flag.String("body", "Hello geeks!!!", "send mail body"))
+	auth := smtp.PlainAuth("", *u, *p, host)
+	
+	for i := 0 ; i < *num; i++ {
+		err := smtp.SendMail(host+":"+port, auth, *from, toList, body)
+
+		// handling the errors
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	
+
+	fmt.Println("end")
 }
